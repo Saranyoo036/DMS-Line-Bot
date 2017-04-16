492bb53dca82756fd30f0fc3ee14445dc3b5f9a4
MrKriKri <stura.wasit@gmail.com>
2017-04-16 15:44:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+ $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 }
  
  
