53bea679f1f495115f96c220f74a7d62086cfb81
MrKriKri <stura.wasit@gmail.com>
2017-04-19 14:27:00 +0700

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,6 +1,6 @@
 
 <?php
-//$use=$_GET["use"];
+$use=$_GET["use"];
  $ID=$_GET["ID"];
  $name=$_GET["name"];
  $level = $_GET["level"];
@@ -15,7 +15,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "U15c9dca3b2a9c602e02889a98c928128";
+$arrPostData['to'] = ".$use.";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "patient ID : ".$ID." Name : ".$name."     Level : ".$level." BloodSugar: ".$BS." Time : ".date("H:i:s")." Date : ".date("Y-m-d")." ";
  
